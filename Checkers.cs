c39b390e68f6f8fbf66175fd1cb8f0a4e1aa436c
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-09-14 15:05:52 -0400

    Checker Board

--- Checkers.cs
+++ Checkers.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Checkers
 {
-    public partial class Form1 : Form
+    public partial class Checkers : Form
     {
-        public Form1()
+        public Checkers()
         {
             InitializeComponent();
+        }
+
+        private void Checkers_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
